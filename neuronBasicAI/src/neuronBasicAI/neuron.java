6ceaa490ba01da00afeab552de9fb04fc97de9cf
Cesare Papa <papacesare05@gmail.com>
2023-11-14 13:01:40 +0100

    Modifica neurone
    1. creazione costruttore neurone e creazione funzione changeWeight()

--- neuronBasicAI/src/neuronBasicAI/neuron.java
+++ neuronBasicAI/src/neuronBasicAI/neuron.java
@@ -1,5 +1,20 @@
 package neuronBasicAI;
 
-public class neuron {
+import java.util.Random;
 
+public class neuron {
+	Random random;
+	float eps;
+	float weight;
+	
+	public neuron() {
+		random = new Random();
+		eps = (float) 1e-3;
+		weight = (float) random.nextGaussian();
+	};
+	
+	public void changeWeight(float error) {
+		//weight = 
+	}
+	
 }
