4586311ad368cf197dcb3ffb25d6ed1dc0f8e193
Cesare Papa <papacesare05@gmail.com>
2023-11-14 20:21:01 +0100

    Aggiornamento dataset di addestramento neurone

--- neuronBasicAI/src/neuronBasicAI/test.java
+++ neuronBasicAI/src/neuronBasicAI/test.java
@@ -3,15 +3,23 @@ package neuronBasicAI;
 public class test {
 	
 	public static void main(String[] args) {
-		neuron n1 = new neuron();
-		float n1Calc = 0;
-		float in = 1f;
-		float out = 10f;
-		
-		while((out + 0.05) > n1Calc && (out - 0.05) < n1Calc || n1Calc != out) {
-			n1Calc  = n1.calculate(in);
-			System.out.println("n1Calc: " + n1Calc);
-		}
+		  neuron n1 = new neuron();
+		  float n1Calc = 0;
+		  float in;
+		  float out;
+		  float[] inputs = {1, 2, 3};
+		  float[] outputs = {0.5f, 1, 1.5f};
+
+		  for(int j = 0; j < 1000; j++) {
+			  for(int i = 0; i < inputs.length; i++) {
+				    in = inputs[i];
+				    out = outputs[i];
+				    n1Calc  = n1.calculate(in);
+				    System.out.println("n1Calc(" + out + "): " + n1Calc);
+				    n1.changeWeight((out - n1Calc), n1Calc);
+				  }
+		  }
+		  
 	}
 
 }
