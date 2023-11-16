692c2e5fda9d4ab53c650dfe5395f89cbb3ac541
Cesare Papa <papacesare05@gmail.com>
2023-11-16 10:21:51 +0100

    Sincronization

--- neuronBasicAI/src/neuronBasicAI/test.java
+++ neuronBasicAI/src/neuronBasicAI/test.java
@@ -1,13 +1,13 @@
 package neuronBasicAI;
-import java.util.List;
-import java.util.ArrayList;
+
+import java.util.*;
 
 public class test {
 	
 	public static void main(String[] args) {
-		/*
+
 		NeuralNetwork scervelo = new NeuralNetwork();
-		scervelo.setnWeightsXNeuron(2);
+		scervelo.setnWeightsXNeuron(1);
 		scervelo.addLayer();
 		scervelo.addNeuronToLayer(0);
 		scervelo.addNeuronToLayer(0);
@@ -16,18 +16,21 @@ public class test {
 		
 		
 		
-		List<List<Float>> xorTrain = new ArrayList<>();
+		List<List<Float>> TrainIn = new ArrayList<>();
+		List<List<Float>> TrainOut = new ArrayList<>();
 
-        xorTrain.add(List.of(0.0f, 0.0f, 0.0f));
-        xorTrain.add(List.of(1.0f, 0.0f, 1.0f));
-        xorTrain.add(List.of(0.0f, 1.0f, 1.0f));
-        xorTrain.add(List.of(1.0f, 1.0f, 0.0f));
+        TrainIn.add(List.of(1f));
+        TrainIn.add(List.of(2f));
+        TrainIn.add(List.of(3f));
+        
+        TrainOut.add(List.of(100f));
+        TrainOut.add(List.of(200f));
+        TrainOut.add(List.of(300f));
 		
 		
 		
-		scervelo.train(xorTrain, 10);
+		scervelo.train(TrainIn, TrainOut, 10000);
 		
-		*/
 		
 		neuron n1 = new neuron(1);
 		float n1Calc = 0;
@@ -37,7 +40,7 @@ public class test {
 		float[] outputs = {2, 4, 6};
 		
 		//training
-		for(int j = 0; j < 100000; j++) {
+		for(int j = 0; j < 1000000; j++) {
 			
 			//cost/error function
 			for(int i = 0; i < inputs.length; i++) {
@@ -51,14 +54,14 @@ public class test {
 		  
 		//result
 		System.out.println("------------------------------ RESULT");
-		System.out.println("weight:" + n1.getWeight(0) + " error: " + (out - n1Calc));
+		System.out.println("weight: " + n1.getWeight(0) + " error: " + (out - n1Calc));
 		
 		for(int i = 0; i < inputs.length; i++) {
 			in = inputs[i];
 			out = outputs[i];
 			n1Calc  = n1.calculate(in);
 			System.out.println("expected:" + out + " actual: " + n1Calc);
-			n1.changeWeight((out - n1Calc), n1Calc);
+			//n1.changeWeight((out - n1Calc), n1Calc);
 		}
 		
 	}
